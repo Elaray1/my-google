18398c33f869bab806891147dc8d61e958ecee2b
Egor <5642452@mail.ru>
2020-06-05 00:50:05 +0300

    Updates

---
+++ precache-manifest.ed3367272e14d0a9825e9f95b88fb561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea847398dbac670d0917d207d351bb6c",
+    "url": "/my-google/index.html"
+  },
+  {
+    "revision": "fa00740e499a86363f74",
+    "url": "/my-google/static/css/main.0df82d82.chunk.css"
+  },
+  {
+    "revision": "6dd52e58c257faf6b287",
+    "url": "/my-google/static/js/2.0f2353aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-google/static/js/2.0f2353aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa00740e499a86363f74",
+    "url": "/my-google/static/js/main.3da84e22.chunk.js"
+  },
+  {
+    "revision": "bde8c8f858a391433d67",
+    "url": "/my-google/static/js/runtime-main.982d2f33.js"
+  },
+  {
+    "revision": "e39ad943b94d3e64ade263af3a2298bf",
+    "url": "/my-google/static/media/arial.e39ad943.ttf"
+  }
+]);
